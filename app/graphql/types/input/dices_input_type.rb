1ace66d85c525bfe5f1883ebff603d99e32d5a5d
bsylla <babacar@gdpr-central.eu>
2022-03-24 00:29:21 +0100

    correct regexp for input validation;

--- app/graphql/types/input/dices_input_type.rb
+++ app/graphql/types/input/dices_input_type.rb
@@ -3,10 +3,10 @@ module Types
     class DicesInputType < Types::BaseScalar
 
       def self.coerce_input(input_value, context)
-        re = Regexp.new('(-?)(\d*)(d|D)(\d*)').freeze
-        
+        re = Regexp.new('(-?)(\d+)(d|D)(\d+)').freeze
+
         if input_value.is_a?(String) and re =~ input_value
-           dice = input_value.split('d')
+           dice = input_value.downcase.split('d')
           [{'value':  dice[0].to_i, 'face': "d"+dice[1]}]
         else
           raise GraphQL::CoercionError, "#{input_value.inspect} is not a valid dices"
@@ -14,7 +14,7 @@ module Types
       end
 
       def self.coerce_result(ruby_value, context)
-        dice = ruby_value.split('d')
+        dice = ruby_value.downcase.split('d')
         [{'value': int(dice[0]), 'face': "d"+dice[1]}]
       end
     end
